ed4d675b4ac1cf037ba93b531a85acbf3e0eca8b
winvirg <thiagowinvirg@gmail.com>
2024-05-16 18:33:29 -0300

    marcar como concluido funcionando

--- task-tracker/src/App.jsx
+++ task-tracker/src/App.jsx
@@ -33,8 +33,14 @@ function App() {
     const newTodos = [...todos]
     const filteredTodos = newTodos.filter(todo => 
       todo.id !== id ? todo : null);
+    
     setTodos(filteredTodos);
+  }
 
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodos);
   }
 
   return (
@@ -42,7 +48,7 @@ function App() {
       <h1>Lista de tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => (
-          <Todo key={todo.id} todo={todo} removeTodo={removeTodo}/>
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
         ))}
       </div>
       <TodoForm addTodo={addTodo}/>
